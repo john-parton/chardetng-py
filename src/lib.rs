869408aacf3fbc8557cfe70678b1a7eb4dd8d39f
John Parton <john.parton.iv@gmail.com>
2023-10-13 15:12:27 -0500

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ fn _fix_encoding_name(encoding: &str) -> &str {
 }
 
 #[doc = include_str!("../chardetng_docs/EncodingDetector.md")]
-#[pyclass(name="EncodingDetector")]
+#[pyclass(name = "EncodingDetector")]
 struct EncodingDetectorWrapper {
     encoding_detector: EncodingDetector,
 }
@@ -28,7 +28,7 @@ impl EncodingDetectorWrapper {
     #[new]
     fn new() -> Self {
         EncodingDetectorWrapper {
-            encoding_detector: EncodingDetector::new()
+            encoding_detector: EncodingDetector::new(),
         }
     }
 
@@ -41,25 +41,17 @@ impl EncodingDetectorWrapper {
     #[doc = include_str!("../chardetng_docs/guess.md")]
     #[pyo3(signature=(*, tld, allow_utf8))]
     fn guess(&self, tld: Option<&[u8]>, allow_utf8: bool) -> &'static str {
-
-        _fix_encoding_name(
-            self.encoding_detector.guess(tld, allow_utf8).name()
-        )
+        _fix_encoding_name(self.encoding_detector.guess(tld, allow_utf8).name())
     }
 
     #[doc = include_str!("../chardetng_docs/guess_assess.md")]
     #[pyo3(signature=(*, tld, allow_utf8))]
     fn guess_assess(&self, tld: Option<&[u8]>, allow_utf8: bool) -> (&'static str, bool) {
-
         let (encoding, higher_score) = self.encoding_detector.guess_assess(tld, allow_utf8);
 
-        (
-            _fix_encoding_name(encoding.name()),
-            higher_score
-        )
+        (_fix_encoding_name(encoding.name()), higher_score)
     }
 }
-
 
 #[pymodule]
 fn detector(_py: Python, m: &PyModule) -> PyResult<()> {
