72aed0d0ddf18778c4987b8b40af6522ec9d146c
Rob Brackett <rob@robbrackett.com>
2023-10-26 14:29:32 -0700

    Return `gb18030` instead of `gbk`
    Chardetng doesn't differentiate between `gbk` and `gb18030` encodings because it is designed to work with encoding_rs, which decodes both of them the same. However, Python's built-in decoders are more strict, and if you try to decode some `gb18030` bytes with the `gbk` decoder, it'll raise an exception. The other way around is fine, though, since `gb18030` is a superset of `gbk`. To make things simpler for users, this translates Chardetng's `gbk` to `gb18030` when passing results back to Python code.

--- src/lib.rs
+++ src/lib.rs
@@ -10,10 +10,10 @@ use pyo3::prelude::*;
 // https://docs.python.org/3/library/codecs.html#standard-encodings
 // https://encoding.spec.whatwg.org/#legacy-single-byte-encodings
 fn _fix_encoding_name(encoding: &str) -> &str {
-    if encoding == "windows-874" {
-        "cp874"
-    } else {
-        encoding
+    match encoding {
+        "windows-874" => "cp874",
+        "GBK" => "gb18030",
+        _ => encoding
     }
 }
 
--- tests/test_decode.py
+++ tests/test_decode.py
@@ -1,7 +1,22 @@
 """Test cases for the chardetng_py module."""
-from chardetng_py import decode
+import pytest
+from chardetng_py import detect
 
 
 def test_decode() -> None:
     """Very basic decode litmus test."""
-    assert decode(b"Jakby r\xeaka Boga") == "Jakby rêka Boga"
+    raw = b"Jakby r\xeaka Boga"
+    assert raw.decode(detect(raw)) == "Jakby rêka Boga"
+
+
+@pytest.mark.parametrize(("text", "encoding", "detected_encoding"), [
+    ("สวัสดี, โลก", "cp874", "cp874"),
+    ("员会 ⛆", "gb18030", "gb18030"),
+])
+def test_returns_python_supported_names(
+    text: str,
+    encoding: str,
+    detected_encoding: str,
+) -> None:
+    """Ensure detection returns encoding names that are supported in Python."""
+    assert detect(text.encode(encoding)) == detected_encoding
